ce2fd603c296c041edcd9eca1b13d634b6e6c319
Aditya Prem Sharma <adityapremsharma@users.noreply.github.com>
2021-03-28 23:20:41 +0530

    fix error

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -10,45 +10,47 @@ export default function Navbar() {
         <i className="fas fa-bars"></i>
       </label>
       <nav className="navbar">
-        <div className="navbar_profile">
-          <img className="navbar_profile-img" alt="logo" src={dp} />
+        <div>
+          <div className="navbar_profile">
+            <img className="navbar_profile-img" alt="logo" src={dp} />
+          </div>
+          <h1 className="navbar_name">Aditya Prem Sharma</h1>
+          <div className="navbar_contact">
+            <a href="https://www.instagram.com/adityaprem99/">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/aditya-prem-sharma/">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+            <a href="mailto:adityapremsharma@gmail.com">
+              <i className="far fa-envelope"></i>
+            </a>
+          </div>
+          <ul className="navbar_list">
+            <Link to="/">
+              <li className="navbar_list-link">
+                <i className="fa fa-home"></i>
+                <span>Home</span>
+              </li>
+            </Link>
+            <Link to="/about">
+              <li className="navbar_list-link">
+                <i className="fa fa-user"></i>
+                <span>About</span>
+              </li>
+            </Link>
+            <Link to="/projects">
+              <li className="navbar_list-link">
+                <i className="fa fa-file-code"></i>
+                <span>Projects</span>
+              </li>
+            </Link>
+            <li className="navbar_list-link">
+              <i className="fa fa-phone-square"></i>
+              <span>Contact</span>
+            </li>
+          </ul>
         </div>
-        <h1 className="navbar_name">Aditya Prem Sharma</h1>
-        <div className="navbar_contact">
-          <a href="https://www.instagram.com/adityaprem99/">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/aditya-prem-sharma/">
-            <i className="fab fa-linkedin-in"></i>
-          </a>
-          <a href="mailto:adityapremsharma@gmail.com">
-            <i className="far fa-envelope"></i>
-          </a>
-        </div>
-        <ul className="navbar_list">
-          <Link to="/">
-            <li className="navbar_list-link">
-              <i className="fa fa-home"></i>
-              <span>Home</span>
-            </li>
-          </Link>
-          <Link to="/about">
-            <li className="navbar_list-link">
-              <i className="fa fa-user"></i>
-              <span>About</span>
-            </li>
-          </Link>
-          <Link to="/projects">
-            <li className="navbar_list-link">
-              <i className="fa fa-file-code"></i>
-              <span>Projects</span>
-            </li>
-          </Link>
-          <li className="navbar_list-link">
-            <i className="fa fa-phone-square"></i>
-            <span>Contact</span>
-          </li>
-        </ul>
       </nav>
     </>
   );
